5a528c6047230f10ab4c98a6993616cad4020a8d
Konrad <konrad.sochacki@Wp.pl>
2023-06-11 20:39:16 +0200

    Update 2.0 Obsługa zapytań z bazy danych mySql - odczytuje dane z tabeli i wyswietla nazwe pobrana na panelu glowny problem to ze nie da sie ustawic kilku scen bo nie lapie nazwy uzytkownika

--- src/main/java/com/example/serwer/HelloApplication.java
+++ src/main/java/com/example/serwer/HelloApplication.java
@@ -2,7 +2,9 @@ package com.example.serwer;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,13 +13,14 @@ import java.sql.DriverManager;
 import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-public class HelloApplication extends Application {
 
+public class HelloApplication extends Application {
+    public static String user;
 
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
@@ -25,19 +28,20 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) throws SQLException {
-
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc-travel-managment-system", "root", "root");
-
         Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM klienci WHERE idKlient = 2");
 
-        ResultSet resultSet = statement.executeQuery("select * from users");
+        while (resultSet.next()) {
+            System.out.println("Imie: " + resultSet.getString("Imie"));
+            System.out.println("Nazwisko: " + resultSet.getString("Nazwisko"));
+            System.out.println("Login: " + resultSet.getString("login"));
+            System.out.println("Haslo: " + resultSet.getString("haslo"));
+            System.out.println("Portfel: " + resultSet.getString("portfel"));
+            user = resultSet.getString("Imie");
+            System.out.println("Imie uzytkownika: "+user);
 
-        while(resultSet.next())
-        {
-            System.out.println(resultSet.getString("name"));
         }
-
-
 
         launch();
     }
--- src/main/java/com/example/serwer/HelloController.java
+++ src/main/java/com/example/serwer/HelloController.java
@@ -1,14 +1,54 @@
 package com.example.serwer;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
-public class HelloController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import static com.example.serwer.HelloApplication.user;
+
+public class HelloController implements Initializable {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
     @FXML
-    private Label welcomeText;
+     Label WelcomeLabel;
+
+
+    /*
+    public void switchToMenu(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+       // scene.getStylesheets().add(css);
+        setUsername(username);
+        stage.setScene(scene);
+        stage.show();
+
+
+
+
+    }
+*/
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        WelcomeLabel.setText("Hello "+ user+"!");
     }
 }
