c6efb1b0c0ec55a630b084ae02f735178297f31d
Konrad <konrad.sochacki@Wp.pl>
2023-06-11 18:06:19 +0200

    Update 1.0 Obsługa zapytań z bazy danych mySql - odczytuje z tabeli users dwoch uzytkownikow - Konrad, Piotr

--- src/main/java/com/example/serwer/HelloApplication.java
+++ src/main/java/com/example/serwer/HelloApplication.java
@@ -6,8 +6,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 public class HelloApplication extends Application {
+
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -15,9 +21,24 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc-travel-managment-system", "root", "root");
+
+        Statement statement = connection.createStatement();
+
+        ResultSet resultSet = statement.executeQuery("select * from users");
+
+        while(resultSet.next())
+        {
+            System.out.println(resultSet.getString("name"));
+        }
+
+
+
         launch();
     }
 }
